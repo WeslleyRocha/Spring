3f1cbaa6c6b40f44284ee8d70af4c9a4e12a6ffb
Weslley Rocha <weslley.nx@gmail.com>
2020-10-02 12:11:00 -0300

    Add metodos, Post, Put, Delete

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -1,14 +1,22 @@
 package org.generation.blogPessoal.controller;
 
 import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.generation.blogPessoal.model.Postagem;
 import org.generation.blogPessoal.repository.PostagemRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,18 +32,39 @@ public class PostagemController {
 	
 	@GetMapping //Metodo que sera disparado
 	public ResponseEntity<List<Postagem>> GetAll(){
-		return ResponseEntity.ok(repository.findAll());		
+		return ResponseEntity.ok(repository.findAll());	
 	}
 	
-	@GetMapping ("/{id}")
+	
+	@GetMapping ("/{id}") //Metodos de pesquisa "por ID" 
 	public ResponseEntity<Postagem> GetById(@PathVariable  long id){
 		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
 		
 	
-	@GetMapping ("/titulo/{titulo}")
+	@GetMapping ("/titulo/{titulo}")  //Metodos de pesquisa dentro do titulo "por Titulo" 
 	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo) {
-		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
-		
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));		
 	}
+	
+	
+	@PostMapping //Metodo de postagem. 
+	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	
+	
+	@PutMapping  //Atualização do metodo, "Atualizar Legenda". 
+	public ResponseEntity<Postagem> put (@RequestBody Postagem postagem)
+	{
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}") //Metodo delete "Post" 
+	public void delete (@PathVariable long id)	
+	{
+		repository.deleteById(id);
+	}
+	
 }
