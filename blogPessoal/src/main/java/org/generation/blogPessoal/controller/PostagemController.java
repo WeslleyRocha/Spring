f60f450d9e0fd1781822b3c56a716db5be761314
Weslley Rocha <weslley.nx@gmail.com>
2020-10-06 14:27:53 -0300

    Add Tababelas

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -5,10 +5,15 @@ import org.generation.blogPessoal.model.Postagem;
 import org.generation.blogPessoal.repository.PostagemRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,9 +38,11 @@ public class PostagemController {
 	}
 		
 	
-	@GetMapping ("/titulo/{titulo}")
+	@GetMapping ("/titulo/{titulo}")  //Metodos de pesquisa dentro do titulo "por Titulo" 
 	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo) {
-		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
-		
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));		
 	}
+	
+
+	
 }
