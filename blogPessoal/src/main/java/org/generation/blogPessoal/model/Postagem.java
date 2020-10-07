04141f5f51f6e94db117394e4c658411ed4215a0
Weslley Rocha <weslley.nx@gmail.com>
2020-10-07 15:40:22 -0300

    relacionamento entre Entity

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 
@@ -24,6 +27,7 @@ public class Postagem {
 	private long id;
 	
 	
+
 	@NotNull   //Este campo ñ pode ser "nulo" vazio. 
 	@Size(min = 5, max = 100) //Limite minino / maximo de caracteres
 	private String titulo;
@@ -34,6 +38,12 @@ public class Postagem {
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date = new java.sql.Date(System.currentTimeMillis()); //Obetendo data da postagem "tempo"
+	
+	
+	@ManyToOne // LInk da tabela postagem com a tabela tema
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 	
 	public long getId() {
 		return id;
@@ -66,4 +76,12 @@ public class Postagem {
 		this.date = date;
 	}
 
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
+		
+	}
+	
 }
